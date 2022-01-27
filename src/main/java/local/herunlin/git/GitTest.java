fc530db02faaae0c007ddb5617182e38a9804453
autorainrepl <98512102+autorainrepl@users.noreply.github.com>
2022-01-27 17:42:55 +0800

    want someone to pull after add something

--- src/main/java/local/herunlin/git/GitTest.java
+++ src/main/java/local/herunlin/git/GitTest.java
@@ -8,9 +8,9 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
-        System.out.println("Hello git4!");
         System.out.println("master test");
         System.out.println("hot-fix test!");
         System.out.println("master push!");
+        System.out.println("want someone to pull");
     }
 }
