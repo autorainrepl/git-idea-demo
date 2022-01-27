c619cdb11de7a20ebf261643ca7c82dddcb4aeca
autohe <1518585346@qq.com>
2022-01-27 16:43:25 +0800

    commit with idea, second

--- src/main/java/local/herunlin/git/GitTest.java
+++ src/main/java/local/herunlin/git/GitTest.java
@@ -6,5 +6,6 @@ package local.herunlin.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
